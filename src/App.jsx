d6d6ee893ba9a311a93673a1b810ab5a49b362f5
Charlie-UWG <75844445+Charlie-UWG@users.noreply.github.com>
2021-02-21 13:08:29 +0900

    1st

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,39 @@
 import React from "react";
+import "./style.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="inputArea">
+        <input placeholder="TODO" type="text" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <div>
+          <ul>
+            <li>あああああ</li>
+            <button>完了</button>
+            <button>削除</button>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>いいいいいい</li>
+            <button>完了</button>
+            <button>削除</button>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <p>完了したTODO</p>
+        <div>
+          <ul>
+            <li>うううううう</li>
+            <button>戻す</button>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 };
